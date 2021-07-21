eb8c12cf55c2b59d19f32c40c91def574328b777
Maksym Harbar <maksym.harbar@skmgroup.pl>
2021-07-21 16:40:33 +0200

    Feature ( Invoices ) Template Structure

--- wp-content/themes/starter/layouts/page-invoices.php
+++ wp-content/themes/starter/layouts/page-invoices.php
@@ -1,17 +1,13 @@
 <?php if ( have_posts() ) : ?>
 	<?php while ( have_posts() ) : the_post(); ?>
 	
-	<article>
-	
 		<?php if ( !$this['config']->get('title_section') ) : ?>
 		
 			<h1 class="gm_page-title uk-h3"><span><?php the_title(); ?></span></h1>
 		
 		<?php endif; ?>
-	
+		
 		<?php the_content(''); ?>
-	
-	</article>
 	
 	<?php endwhile; ?>
 	
