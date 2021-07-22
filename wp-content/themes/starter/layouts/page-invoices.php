b019bee1de0e9a84d2d479b89f4a305f47b54685
Maksym Harbar <maksym.harbar@skmgroup.pl>
2021-07-22 11:52:46 +0200

    Feature ( Invoices - Page )

--- wp-content/themes/starter/layouts/page-invoices.php
+++ wp-content/themes/starter/layouts/page-invoices.php
@@ -1,11 +1,17 @@
 <?php if ( have_posts() ) : ?>
 	<?php while ( have_posts() ) : the_post(); ?>
-	
+		
+		<div class="gm_invoices-page">
+		
 		<?php if ( !$this['config']->get('title_section') ) : ?>
 		
-			<h1 class="gm_page-title uk-h3"><span><?php the_title(); ?></span></h1>
+			<h1 class="gm_invoices-page__title uk-h3"><span><?php the_title(); ?></span></h1>
 		
 		<?php endif; ?>
+		
+		<div class="gm_invoices-page__content uk-margin-top">
+			<?php echo gm_get_invoices(); ?>
+		</div>
 		
 		<?php the_content(''); ?>
 	
