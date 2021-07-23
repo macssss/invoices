f7e29f62585fc41af827d79e818da5314bf1e0e4
Maksym Harbar <maksym.harbar@skmgroup.pl>
2021-07-23 09:09:03 +0200

    Feature ( Invoices - Pagination ) Add styles and layout improvments

--- wp-content/themes/starter/invoices/pagination.php
+++ wp-content/themes/starter/invoices/pagination.php
@@ -10,35 +10,59 @@
 
 <div class="gm_invoices__pagination gm_invoices-pagination">
 	
-	<ul class="gm_invoices-pagination__list">
-		
-		<?php if ( $pagination_count > 1 && $current > 1 ): ?>
-		
-			<li><a class="gm_invoices-pagination__item gm_invoices-pagination__item--prev" href="#page-<?php echo $current-1; ?>">Prev</a></li>
-		
-		<?php endif; ?>
+	<div class="gm_invoices-pagination__inner">
 		
 		
-		<?php 
+		<div class="gm_invoices-pagination__current-wrap">
 			
-			for ( $i = 1; $i <= $pagination_count; $i++ ) {
-				
-				$item_class = $i == $current ? ' gm_invoices-pagination__item--active' : '';
-				
-				echo '<li><a class="gm_invoices-pagination__item' . $item_class . '" href="#page-' . $i . '">' . $i . '</a></li>';
-				
-			}
-		
-		?>
+			<div class="gm_invoices-pagination__current"><?php printf( '%s %s %s %s', __( 'Page', 'wrap' ) , $current, __( 'of', 'wrap' ), $pagination_count ); ?></div>
+			
+		</div>
 		
 		
-		<?php if ( $pagination_count > 1 && $current < $pagination_count ): ?>
 	
-			<li><a class="gm_invoices-pagination__item gm_invoices-pagination__item--next" href="#page-<?php echo $current+1; ?>">Next</a></li>
+		<div class="gm_invoices-pagination__list-wrap">
+			
+			<ul class="gm_invoices-pagination__list">
+				
+				<?php if ( $pagination_count > 1 && $current > 1 ): ?>
+				
+					<li><a class="gm_invoices-pagination__item gm_invoices-pagination__item--prev" href="#page-<?php echo $current - 1; ?>"></a></li>
+				
+				<?php endif; ?>
+				
+				
+				<?php 
+					
+					for ( $i = 1; $i <= $pagination_count; $i++ ) {
+						
+						if ( $i == $current ) {
+							
+							echo '<li><span class="gm_invoices-pagination__item gm_invoices-pagination__item--active">' . $i . '</a></li>';
+						}
+						
+						else {
+							
+							echo '<li><a class="gm_invoices-pagination__item" href="#page-' . $i . '">' . $i . '</a></li>';
+						}
+						
+					}
+				
+				?>
+				
+				
+				<?php if ( $pagination_count > 1 && $current < $pagination_count ): ?>
+			
+					<li><a class="gm_invoices-pagination__item gm_invoices-pagination__item--next" href="#page-<?php echo $current + 1; ?>"></a></li>
+				
+				<?php endif; ?>
+				
+				
+			</ul>
+			
+		</div>
 		
-		<?php endif; ?>
-		
-		
-	</ul>
+	
+	</div>
 	
 </div>
